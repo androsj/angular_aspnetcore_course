d61fb5aa4743f691db00257f3250398b8a59ea39
Andros Rosa <andros2011@gmail.com>
2017-08-21 18:16:20 -0400

    Add Vehicles to domain model

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -5,13 +5,17 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
             
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature> Features { get; set; }
-        //public DbSet<Model> MyProperty { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => new { vf.VehicleId, vf.FeatureId });
+        }
     }
 }

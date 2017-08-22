dd0dcd4dfdc15235fbbbf5fe2b7289ebc5535086
Andros Rosa <andros2011@gmail.com>
2017-08-22 18:49:37 -0400

    Add vehicle to database
    ** DAMMIT im stupid **

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,24 +1,37 @@
+using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
+        private readonly VegaDbContext context;
         private readonly IMapper mapper;
-        public VehiclesController(IMapper mapper)
+
+        public VehiclesController(VegaDbContext context, IMapper mapper)
         {
+            this.context = context;
             this.mapper = mapper;
         }
 
         [HttpPost]
-        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+            
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
         }
     }
 }

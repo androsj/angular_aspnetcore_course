352b4d150e55db89d57d33a1d86db375d6dc6e42
Andros Rosa <andros2011@gmail.com>
2017-08-22 22:27:50 -0400

    Simplify Resources and change result format to full vehicle resource

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -6,11 +6,11 @@ namespace vega.Controllers.Resources
 {
     public class VehicleResource
     {
-                public int Id { get; set; }
+        public int Id { get; set; }
 
-    public MakeResource Make { get; set; }
+        public KeyValuePairResource Make { get; set; }
 
-        public ModelResource Model { get; set; }
+        public KeyValuePairResource Model { get; set; }
 
         public bool IsRegistered { get; set; }
 
@@ -18,11 +18,11 @@ namespace vega.Controllers.Resources
 
         public DateTime LastUpdate { get; set; }
 
-        public ICollection<FeatureResource> Features { get; set; }
+        public ICollection<KeyValuePairResource> Features { get; set; }
 
         public VehicleResource()
         {
-            Features = new Collection<FeatureResource>();
+            Features = new Collection<KeyValuePairResource>();
         }
     }
 }

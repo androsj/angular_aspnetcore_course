b969d4a3885852910f511a3bad9684c56e8edfbb
Andros Rosa <andros2011@gmail.com>
2017-11-16 00:25:31 -0400

    Calculate total size of query result

--- ClientApp/app/components/shared/pagination.component.ts
+++ ClientApp/app/components/shared/pagination.component.ts
@@ -20,7 +20,7 @@ import { OnChanges } from '@angular/core';
                 </a>
             </li>
         </ul>
-    </nav>  
+    </nav>
 `
 })
 export class PaginationComponent implements OnChanges {
@@ -28,12 +28,12 @@ export class PaginationComponent implements OnChanges {
 	@Input('page-size') pageSize = 10;
 	@Output('page-changed') pageChanged = new EventEmitter();
 	pages: any[];
-	currentPage = 1; 
+	currentPage = 1;
 
 	ngOnChanges() {
     	this.currentPage = 1;
-        
-		var pagesCount = Math.ceil(this.totalItems / this.pageSize); 
+
+		var pagesCount = Math.ceil(this.totalItems / this.pageSize);
 		this.pages = [];
 		for (var i = 1; i <= pagesCount; i++)
 			this.pages.push(i);
@@ -42,7 +42,7 @@ export class PaginationComponent implements OnChanges {
 	}
 
 	changePage(page) {
-		this.currentPage = page; 
+		this.currentPage = page;
 		this.pageChanged.emit(page);
 	}
 
@@ -56,8 +56,8 @@ export class PaginationComponent implements OnChanges {
 
 	next() {
 		if (this.currentPage == this.pages.length)
-			return; 
-		
+			return;
+
 		this.currentPage++;
     	console.log("next", this);
 		this.pageChanged.emit(this.currentPage);
